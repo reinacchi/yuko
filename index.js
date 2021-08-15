11e59515b403bf367c2e34bf41090eae5647175e
Reinhardt <contact@reinhello.xyz>
2021-08-15 17:19:58 +0700

    Export classes

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 
 module.exports = {
     Client: require("./lib/client/Client"),
+    ClientUser: require("./lib/structures/ClientUser"),
+    RESTManager: require("./lib/rest/RESTManager"),
+    User: require("./lib/structures/User")
 };
