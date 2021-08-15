8fbaf6b47f72154a85d6eb7fb79dc89f42ad6ab0
Reinhardt <notmarx.tech@gmail.com>
2021-08-15 16:37:41 +0700

    Initial release

--- README.md
+++ README.md
@@ -2,4 +2,39 @@
 
 A Discord Library Written In JavaScript.
 
-- Note: This library is very W.I.P. A better README will be work on once the library is finished :)
+- **Note:** This library is W.I.P and can be use for testing purpose only.
+
+# Installing
+
+You'll need NodeJS 14+ installed.
+
+```
+npm i --save yuko
+```
+
+# Examples
+
+This library currently doesn't support most Gateway event for now and there's nothing else you can do other than this example below.
+
+- More examples can be found [Here](https://github.com/NotMarx/yuko/tree/master/examples).
+
+```js
+const Yuko = require("yuko");
+const client = new Yuko.Client("Bot TOKEN"});
+
+client.on("ready", () => {
+    console.log(`${client.user.tag} is Ready!`);
+});
+
+client.connect();
+```
+
+# Resources Links
+
+- **Yuko's Official Docs** is currently **W.I.P**.
+- **[Yuko's GitHub Repository](https://github.com/NotMarx/yuko)** is where the main development of the library.
+- **[Yuko's Support Server](https://discord.gg/5wP5cCqSHD)** is where you can ask help within the library or contact me.
+
+# License
+
+**Yuko** is released under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ index.d.ts
@@ -0,0 +1,127 @@
+import { EventEmitter } from "events";
+
+/**
+ * A Discord Library Written In JavaScript
+ */
+declare module "yuko";
+
+type ContentType = string | "application/json"
+type HTTPMethod = "DELETE" | "GET" | "PATCH" | "POST" | "PUT";
+
+interface ClientEvents {
+    ready: [];
+}
+
+interface ClientOptions {
+    allowedMentions;
+    shard?: boolean;
+    shardCount?: number | "auto";
+}
+
+interface EmbedAuthorOptions {
+    icon_url?: string;
+    name: string;
+    url?: string;
+}
+
+interface EmbedFieldOptions {
+    inline?: boolean;
+    name: string;
+    value: string;
+}
+
+interface EmbedFooterOptions {
+    icon_url?: string;
+    text?: string;
+}
+
+interface EmbedImageOptions {
+    url?: string;
+}
+
+interface EmbedOptions {
+    author?: EmbedAuthorOptions;
+    color?: number;
+    description?: string;
+    fields: EmbedFieldOptions[];
+    footer?: EmbedFooterOptions;
+    image?: EmbedImageOptions;
+    thumbnail?: EmbedImageOptions;
+    timestamps?: string | Date;
+    title?: string;
+    url?: string;
+}
+
+class Base {
+    constructor(id: string);
+
+    createdAt: number;
+    id: string;
+    getCreatedAt(id: string): number;
+    toJSON(props: string[]): { id: string, createdAt: number };
+    toString(): string;
+}
+
+export class Client extends EventEmitter {
+    constructor(token: string, options?: ClientOptions);
+
+    connected: boolean;
+    isSharded: boolean;
+    latencies: number[];
+    latency: number;
+    rest: RESTManager;
+    shards: any[];
+    token: string;
+    user: ClientUser;
+    connect(): Promise<void>
+    on<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => void): this;
+    on<S extends string | symbol>(
+        event: Exclude<S, keyof ClientEvents>,
+        listener: (...args: any[]) => void,
+    ): this;
+    once<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => void): this;
+    once<S extends string | symbol>(
+        event: Exclude<S, keyof ClientEvents>,
+        listener: (...args: any[]) => void,
+    ): this;
+    emit<K extends keyof ClientEvents>(event: K, ...args: ClientEvents[K]): boolean;
+    emit<S extends string | symbol>(event: Exclude<S, keyof ClientEvents>, ...args: any[]): boolean;
+    off<K extends keyof ClientEvents>(event: K, listener: (...args: ClientEvents[K]) => void): this;
+    off<S extends string | symbol>(
+        event: Exclude<S, keyof ClientEvents>,
+        listener: (...args: any[]) => void,
+    ): this;
+    removeAllListeners<K extends keyof ClientEvents>(event?: K): this;
+    removeAllListeners<S extends string | symbol>(event?: Exclude<S, keyof ClientEvents>): this;
+}
+
+export class ClientUser extends User {
+    constructor(client: Client, data: object);
+
+    locale: string;
+    mfaEnabled: boolean;
+    verified: boolean;
+    private loadData(data: object): Promise<void>;
+}
+
+export class User extends Base {
+    constructor(client: Client, data: object);
+    
+    bot: boolean;
+    client: Client;
+    discriminator: string;
+    id: string;
+    system: boolean;
+    tag: string;
+    toString: string;
+    rawData: object;
+    username: string;
+    private loadData(data: object): Promise<void>;
+
+}
+
+export class RESTManager {
+    constructor(client: Client);
+
+    request(route: object, payload?: unknown, contentType: ContentType): Promise<unknown>;
+}
