ff8afa00c165f386990578984a86b917b68326ce
Reinhardt <contact@reinhello.xyz>
2021-08-28 20:48:58 +0700

    0.1.1
    commit 6fc33ff20ad997da992fed2ff868de28a703480f
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Sat Aug 28 20:47:47 2021 +0700
    
        0.1.1
    
    commit e06cbdb2df61dd5440e7e2e4a2c13e39cff6f670
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Fri Aug 27 12:17:15 2021 +0700
    
        Add components typing
    
    commit 001d476860347068e92d0da4ddc5842e8a397a50
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Fri Aug 27 12:01:33 2021 +0700
    
        Update Client, typing, and export class
    
    commit dff6929b8713864d9318121014fa586666d7fc64
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Fri Aug 27 12:01:15 2021 +0700
    
        Add delete method
    
    commit c7235cbaad332d5a1cd114211ceddea1715b0d56
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Fri Aug 27 12:01:07 2021 +0700
    
        Code cleanup
    
    commit 52ad8d1986ad5af6721c19c340c2260c5a491eff
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:08:02 2021 +0700
    
        Various of additions
    
        - Create DM with a user
        - More method and prop
        - Typing additions
        - `guildCreate` event
    
    commit 1cebf181aec479ccefb6958f8d9109c0bb1d4935
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:07:09 2021 +0700
    
        Add method to edit status
    
    commit 2b1d53ed645cdb6854bd5355f342cd1c873aabff
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:06:52 2021 +0700
    
        Reupdate DiscordError
    
    commit 4d14b4fe8d26d78208a8ca0685ace67a21e417b6
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:06:31 2021 +0700
    
        eslint fix
    
    commit 528fabcdab6875203af504415223853689dc0d16
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:06:16 2021 +0700
    
        Update example
    
    commit f1bc52edcbac38c82dae7842efe357b3f51c4eae
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:05:59 2021 +0700
    
        Rename method
    
    commit ac14ab217990284310072136b726c78b413b2105
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:05:48 2021 +0700
    
        Add new classes
    
    commit b69421a3d4778eb65fd422fd8ecee2abd32ae0e6
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Thu Aug 26 21:05:30 2021 +0700
    
        Update README
    
    commit a14b789d022aac399db338606e6f2db9e529073e
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Mon Aug 16 14:00:45 2021 +0700
    
        Update README and version
    
    commit 125067ea44e2ee608b59769ee4064b9432c0243d
    Author: Reinhardt <notmarx.tech@gmail.com>
    Date:   Sun Aug 15 22:24:31 2021 +0700
    
        Update examples

--- examples/basic.js
+++ examples/basic.js
@@ -1,15 +1,27 @@
 const Yuko = require("yuko");
-const client = new Yuko.Client("Bot TOKEN", { intents: ["guilds", "guildMessages"]});
+const client = new Yuko.Client("TOKEN", { intents: ["guilds", "guildMessages"] });
 
 client.on("ready", () => {
-    console.log(`${client.user.username} is Ready!`);
+    console.log(`${client.user.tag} is Ready!`);
 });
 
 client.on("messageCreate", (message) => {
     if (message.author.bot) return;
 
     if (message.content === "!ping") {
-        client.createMessage("The Channel ID", { content: "Pong!"});
+        client.createMessage(message.channelID, { content: "Pong!"});
+    } else if (message.content === "!embed") {
+        client.createMessage(message.channelID, {
+            embeds: [
+                {
+                    title: "This is the title of the embed",
+                    description: "Some cool description I can add to tell!",
+                    color: 0x42B893
+                }
+            ]
+        });
+    } else if (message.content === "!reply") {
+        client.createMessage(message.channelID, { content: "Replied to your message!", messageReference: { messageID: message.id } });
     }
 });
 
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 "use strict";
 
 module.exports = {
+    Channel: require("./lib/structures/Channel"),
     Client: require("./lib/client/Client"),
     ClientUser: require("./lib/structures/ClientUser"),
+    Collection: require("./lib/utils/Collection"),
+    DMChannel: require("./lib/structures/DMChannel"),
+    Guild: require("./lib/structures/Guild"),
+    GuildChannel: require("./lib/structures/GuildChannel"),
+    Member: require("./lib/structures/Member"),
     Message: require("./lib/structures/Message"),
     RESTManager: require("./lib/rest/RESTManager"),
-    User: require("./lib/structures/User")
+    TextChannel: require("./lib/structures/TextChannel"),
+    User: require("./lib/structures/User"),
+    Websocket: require("./lib/utils/Websocket"),
+
+    VERSION: require("./package.json").version
 };
