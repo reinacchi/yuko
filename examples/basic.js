7be004739ee5c93d5be39f6b355b3d9e8196a81b
Reinhardt <contact@reinhello.xyz>
2021-11-24 16:55:57 +0700

    Update examples

--- examples/basic.js
+++ examples/basic.js
@@ -5,6 +5,10 @@ client.on("gatewayReady", () => {
     console.log(`${client.user.username} is Ready!`);
 });
 
+client.on("error", (err) => {
+    console.log(err);
+});
+
 client.on("messageCreate", (message) => {
     if (message.author.bot) {
         return;
@@ -14,13 +18,11 @@ client.on("messageCreate", (message) => {
         message.channel.createMessage({ content: "Pong!" });
     } else if (message.content === "!embed") {
         message.channel.createMessage({
-            embeds: [
-                {
-                    title: "This is the title of the embed",
-                    description: "Some cool description I can add to tell!",
-                    color: 0x42B893
-                }
-            ]
+            embed: {
+                title: "This is the title of the embed",
+                description: "Some cool description I can add to tell!",
+                color: 0x42B893
+            }
         });
     } else if (message.content === "!reply") {
         message.channel.createMessage({ content: "Replied to your message!", messageReference: { messageID: message.id } });
